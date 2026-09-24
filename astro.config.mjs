c74fb95735f1d23616f6255471ca972eaa3fefa6
manu-d3v3lop3r <167571792+manu-d3v3lop3r@users.noreply.github.com>
2026-09-24 15:43:04 +0200

    fix: configure default locale redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
     defaultLocale: "es",
     routing: {
       prefixDefaultLocale: true,
-      redirectToDefaultLocale: false,
+      redirectToDefaultLocale: true,
     },
   },
 });
