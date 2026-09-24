7d9d3dcd8899cd580f02674196b97198c589d05d
manu-d3v3lop3r <167571792+manu-d3v3lop3r@users.noreply.github.com>
2026-09-24 15:27:09 +0200

    ci: configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  site: "https://manu-d3v3lop3r.github.io",
+
   i18n: {
     locales: ["es", "en"],
     defaultLocale: "es",
